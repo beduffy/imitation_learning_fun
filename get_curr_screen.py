3862dec09a5449f09fbe7fa4a09845a90c4a113f
beduffy <benfduffy@gmail.com>
2024-05-05 17:49:51 +0200

    Looping live image live in cv2, meta

--- get_curr_screen.py
+++ get_curr_screen.py
@@ -1,10 +1,22 @@
 import pyautogui
+import cv2
+import numpy as np
 
-def capture_screen(filename='screenshot.png'):
-    # Take a screenshot using pyautogui
-    screenshot = pyautogui.screenshot()
-    # Save the screenshot to a file
-    screenshot.save(filename)
+def show_screen_live():
+    # Continuously capture the screen and display it live
+    while True:
+        # Take a screenshot, convert to numpy array, change color space from RGB to BGR, and resize
+        frame = cv2.cvtColor(np.array(pyautogui.screenshot()), cv2.COLOR_RGB2BGR)
+        width = 1080
+        height = int(frame.shape[0] * (width / frame.shape[1]))
+        frame = cv2.resize(frame, (width, height))
+        # Display the resized frame using OpenCV
+        cv2.imshow("Live Screen", frame)
+        if cv2.waitKey(100) & 0xFF == ord('q'):  # Break the loop when 'q' is pressed
+            break
+
+        
+    cv2.destroyAllWindows()
 
 # Usage
-capture_screen('my_screenshot.png')
+show_screen_live()
